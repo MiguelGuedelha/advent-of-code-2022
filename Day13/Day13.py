ac610a10aaca800782bef34ea6ce3f2de804f2f1
Miguel Guedelha <miguel.guedelha@deptagency.com>
2022-12-13 10:27:36 +0000

    Day 13 - Halfway done

---
+++ Day13/Day13.py
@@ -0,0 +1,48 @@
+﻿from itertools import zip_longest
+import functools
+
+def is_in_right_order(left, right):
+  
+  if type(left) == int and type(right) == int:
+    if left > right:
+      return 'f'
+    if left < right:
+      return 't'
+    return 'c'
+
+  if type(left) == list or type(right) == list:
+    if(type(left) != list):
+      left = [left]
+
+    if(type(right) != list):
+      right = [right]
+    
+    pairs = zip_longest(left, right, fillvalue=None)
+    for (leftItem, rightItem) in pairs:
+      if(leftItem is None):
+        return True
+      if(rightItem is None):
+        return False  
+      
+      result = is_in_right_order(leftItem, rightItem)
+      if result == 'c':
+        continue
+      else:
+        return True if result == 't' or result == True else False
+    return 'c'
+
+f = open("input.txt", "r", encoding='utf-8-sig')
+
+input = f.read()
+
+pairs = '\n'.join(input.split('\n\n')).split('\n')
+
+pairs = [eval(pair) for pair in pairs]
+
+pairs.append([[2]])
+pairs.append([[6]])
+
+sorted = sorted(pairs, key=functools.cmp_to_key(is_in_right_order))
+
+
+print(sorted)
--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,5 +1,56 @@
-﻿using System.Text;
+﻿using OneOf;
+using System.Text;
 
 using var reader = new StreamReader("input.txt", Encoding.UTF8);
 
 var inputString = reader.ReadToEnd();
+
+var result = new List<int>();
+
+var pairs = inputString
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(x => x.Split(Environment.NewLine))
+    .ToList();
+
+
+for (var i = 0; i < pairs.Count; i++)
+{
+    var leftChars = pairs[i][0].ToCharArray();
+    var leftBorders = ArrayBorders(leftChars);
+    var leftArray = new List<dynamic>();
+
+    var rightChars = pairs[i][1].ToCharArray();
+    var rightBorders = ArrayBorders(rightChars);
+    var rightArray = new List<dynamic>();
+
+
+    for (int j = 0; j < leftChars.Length; j++)
+    {
+
+    }
+}
+
+List<(int, int)> ArrayBorders(char[] chars)
+{
+    var starts = new Stack<int>();
+    var result = new List<(int, int)>();
+    for (var i = 0; i < chars.Length; i++)
+    {
+        if (chars[i] == '[')
+        {
+            starts.Push(i);
+        }
+
+        if (chars[i] == ']')
+        {
+            result.Add((starts.Pop(), i));
+        }
+    }
+
+    return result.OrderBy(x => x.Item1).ToList();
+}
+
+class Element
+{
+    public OneOf<int, Element[]> Value { get; set; }
+}
